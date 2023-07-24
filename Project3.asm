e2994252dcbb291154bd44efaf9b98aac1510d22
calderpru <131316452+calderpru@users.noreply.github.com>
2023-07-23 22:20:57 -0700

    Create Project3.asm

---
+++ Project3.asm
@@ -0,0 +1,37 @@
+TITLE Program Template     (Proj3_prulhiec.asm)
+
+; Author: Calder Prulhiere
+; Last Modified: 7/23/2023
+; OSU email address: prulhiec@oregonstate.edu
+; Course number/section:   CS271 Section 400
+; Project Number: 3                Due Date: 7/23/2023
+; Description: Data Validation, Looping, and Constants
+
+INCLUDE Irvine32.inc
+
+
+
+
+
+
+
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
